242e300e8c572cd107ef1a27de3f47f1b02253b3
shawn <shawn@rsatechnologyservices.com>
2024-07-19 23:54:38 -0500

    App UI mostly laid out. 3 functions created 2 functions partially functional

--- js/site.js
+++ js/site.js
@@ -1,12 +1,55 @@
 function getValues(){
- alert("Hello App!!!");
+
+    let loanAmountInput = document.getElementById("loanAmount").value; 
+    let termOfMonthsInput = document.getElementById("termOfMonths").value; 
+    let loanInterestRateInput = document.getElementById("loanInterestRateInput").value; 
+
+    //call calulation funtion(maths)
+
+    //verify inputs that everything that is a decimal number.
+    displayStuff();
 }
 
-//get values from the screen and display them
-function getValues2(){
-    let inputMessage = document.getElementById("message").value; 
+function maths(){
 
-    let alertMessage = document.getElementById("alertMessage");
+//math() library(?) object(?) method
 
-    alertMessage.innerText = inputMessage;
+// Total Monthly Payment = (amount loaned) * (rate / 1200) / ( 1 - (1 + rate/1200)^(-Number of Months) )
+
+// Before the very first month, Remaining Balance = the amount of the loan
+
+// Interest Payment = Previous Remaining Balance * rate/1200
+
+// Principal Payment = Total Monthly Payment - Interest Payment
+
+// At the end of each month, Remaining Balance = Previous Remaining Balance - Principal Payment
+
+}
+
+function displayStuff(){
+    let template = document.getElementById("amortizedTable-Template");
+    let tableBody = document.getElementById("amortizedTableBody");
+
+    //Clear the table data first.
+    amortizedTableBody.innerHTML = "";
+
+  //Loop over the items and display them.
+    for(let index=0; index < moneys.length; index ++){
+    let moneyEvent = moneys[index];
+
+    //Get a document fragment from the template.
+    let moneyRow = document.importNode(template.content, true);
+
+    //Select the td based on an attribute.
+    moneyRow.querySelector("[month-data]").textContent = moneyEvent.month;
+    moneyRow.querySelector("[payment-data]").textContent = moneyEvent.city;
+    moneyRow.querySelector("[principal-data]").textContent = moneyEvent.state;
+    moneyRow.querySelector("[interest-data]").textContent = moneyEvent.attendance;
+    moneyRow.querySelector("[totalInterest-data]").textContent = moneyEvent.attendance;
+    moneyRow.querySelector("[balance-data]").textContent = moneyEvent.attendance;
+
+    
+
+    amortizedTableBody.appendChild(moneyRow);
+  }
 }
